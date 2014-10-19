e5349493c9784ac51266291574b48aa1b7c68f51
agnieszkar <agnieszkar2012@gmail.com>
2014-10-19 11:30:21 +0200

    Zmieniłam ilość iteracji

--- GitRepo/src/Main.java
+++ GitRepo/src/Main.java
@@ -2,8 +2,8 @@
 public class Main {
 	public static void main (String[] args)
 	{
-		//Agnieszka
-		for(int i = 0; i < 10; ++i)
+		//Agnieszka 
+		for(int i = 0; i < 5; ++i)
 		{
 			System.out.println("Hello new git!");
 		}
